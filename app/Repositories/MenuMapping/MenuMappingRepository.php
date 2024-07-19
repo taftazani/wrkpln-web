8485eaf6869d95b924d4ad35cbe65c6552be7256
ilhamrafi44 <ilhamrafi44@gmail.com>
2024-07-19 20:50:22 +0700

    Create Command for Creating Services and Repositories

---
+++ app/Repositories/MenuMapping/MenuMappingRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\MenuMapping;
+
+class MenuMappingRepository
+{
+        // Your repository methods go here
+}
---
+++ app/Services/MenuMapping/MenuMappingService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\MenuMapping;
+
+class MenuMappingService
+{
+        // Your service methods go here
+}
